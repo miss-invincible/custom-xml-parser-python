529a2faeaed1be7e9023bb027d9c61e2ada502d3
shivangi <shivangi.gupta.jiit@mgail.com>
2016-04-28 19:23:18 +0530

    made improvements

--- xml_parser.py
+++ xml_parser.py
@@ -1,24 +1,46 @@
-xml_string = "<note><to>Tove</to><from>Jani</from><heading>Reminder</heading><body>Don't forget me this weekend!</body></note>"
+xml_string = "<note><to>Tove</to><from>Jani</from><heading><test><deep>Reminder</deep></test></heading><body>Don't forget me this weekend!</body></note>"
 
 cur_pos = 0;
-
+level=0
 
 class node(object):
+    
     def __init__(self, value, children = []):
         self.value = value
         self.children = children
 
-    def res(self):
-    	print self.value,self.children
+    def res(self,level=0):
+    	
+    	count = 1
+    	
+    	for val in self.children:
+    		strg = ""
+    		if not val == None:
+    			for i in range(0,level):
+    				strg = strg+"	"
+
+    			print strg,"parent = ",self.value
+    			print strg,"child number= ",count
+    			print strg,"child name=", val.value
+    			print "\n"
+    			count+=1;
+    		
+    		val.res(level+1)
+    			
+
 
 
 
 
 def check_for_open():
 	global cur_pos
+
+	if cur_pos>= len(xml_string):
+		return [0,'']
+
 	start=cur_pos+1
 	signal = 0
-	#name = "lol"
+	
 	if(xml_string[cur_pos]=='<'):
 		signal=1
 		if not xml_string[cur_pos+1]=='/':
@@ -27,25 +49,27 @@ def check_for_open():
 				cur_pos+=1;
 		else:
 			result = [0,'']
-			#print 'ok'
+			
 			return result
 
 	else:
 		result = [0,'']
-		#print result
+		
 		return result
 
 	if(signal==1):
 		name = xml_string[start:cur_pos]
 
 	result = [signal,name]
-	#print result
 	return result
 
 
 
 def check_for_close():
 	global cur_pos
+	if cur_pos>= len(xml_string):
+		return [0,'']
+
 	start = cur_pos+2
 	signal = 0
 	name = ""
@@ -67,8 +91,6 @@ def check_for_close():
 
 	result= [signal,name]
 
-	print result
-
 	return result
 
 
@@ -81,41 +103,40 @@ def xml_parser(name):
 	global cur_pos
 	children = []
 	
-	while not xml_string[cur_pos]=='<' :
-		cur_pos+=1
+	while(cur_pos<len(xml_string)):
+		while cur_pos<len(xml_string) and not xml_string[cur_pos]=='<' :
+			if cur_pos < len(xml_string):
+				cur_pos+=1
+			else:
+				return null
 
-	print "kk",cur_pos
-	a = check_for_open()
-	#cur_pos+=1
-	#print 'ff',cur_pos
-	#while not xml_string[cur_pos]=='<' :
-	#	cur_pos+=1
-	#print cur_pos,"yy"
 
-	b = check_for_close()
-	print "->",a
-	print cur_pos
-	print "-->",b
+		
+		a = check_for_open()
+		if(cur_pos <len(xml_string)):
+			if a[0]==0:
+				b = check_for_close()
+				if b[0]==1:
+					result = node(name,children)
+					return result
+			else:
+				cur_pos+=1
+				if cur_pos < len(xml_string):
+					children.append(xml_parser(a[1]))
 
-	if a[0]==1:
-		cur_pos+=1
-		print cur_pos
-		print 'wrong'
-		if cur_pos < len(xml_string):
-			children.append(xml_parser(a[1]))
+				else:
+					result = node(name,children)
+					return result
 
 		else:
-			result = node(name,children)
-			
+			result =node (name,children)
 			return result
-
-	if b[0]==1:
-		result = node(name,children)
-		return result
 		
 
 
-#cur_pos=14
-#check_for_close()
-xml_parser("root")
+a = xml_parser("root")
+a.res()
+
+
+
 
